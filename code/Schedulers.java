dcc77b111b59c4fe1b1ee7c8c179aefef9bd4c99
Yousef-olx <160499310+Yousef-olx@users.noreply.github.com>
2024-11-07 19:45:24 +0300

    Update Schedulers.java

--- code/Schedulers.java
+++ code/Schedulers.java
@@ -1,30 +1,21 @@
 public class Schedulers {
     private queue finish;
     public void First_Come_First_Serve(queue ready){
-int currenttime=0;
+         int currenttime=0;
     	 node N1 = new node();
-    	 int memoerycalc=0;
-    	 queue readyQueue=ready;
-
-
-    	for(int i=0; i<readyQueue.length();i++){
-    	    if(N1.process.getMemory()>1024){
-    	    N1.setProcess(null);
-    	    }
-    	    readyQueue.serve();
-    	   
-    	}
+    	queue readyQueue=ready;
+        
     	for(int i=0;i<readyQueue.length();i++){
+    	    
     		N1.process.setWait(currenttime);
 
-            N1.process.setTurnaround(currenttime + N1.process.getBurst());
+    	   N1.process.setTurnaround(currenttime + N1.process.getBurst());
 
     	    currenttime+=N1.process.getBurst();
-    	    
-    	    memoerycalc+= N1.process.getMemory();
+    	   
+    	N1.process.setState( N1.process.getState().finish);
 
-
-    	    readyQueue.serve();
+            finish.enqueue(readyQueue.serve());
 
     	}// for now 
     }
