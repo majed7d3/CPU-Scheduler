2a5664ec7ce117832446b3f9e8e88a9d7a8bdaff
Yousef-olx <160499310+Yousef-olx@users.noreply.github.com>
2024-11-07 16:49:53 +0300

    Update Schedulers.java

--- code/Schedulers.java
+++ code/Schedulers.java
@@ -1,7 +1,32 @@
 public class Schedulers {
     
     public void First_Come_First_Serve(queue ready){
+int currenttime=0;
+    	 node N1 = new node();
+    	 int memoerycalc=0;
+    	 queue readyQueue=ready;
 
+
+    	for(int i=0; i<readyQueue.length();i++){
+    	    if(N1.process.getMemory()>1024){
+    	    N1.setProcess(null);
+    	    }
+    	    readyQueue.serve();
+    	   
+    	}
+    	for(int i=0;i<readyQueue.length();i++){
+    		N1.process.setWait(currenttime);
+
+            N1.process.setTurnaround(currenttime + N1.process.getBurst());
+
+    	    currenttime+=N1.process.getBurst();
+    	    
+    	    memoerycalc+= N1.process.getMemory();
+
+
+    	    readyQueue.serve();
+
+    	}// for now 
     }
 
     public void Round_Robin(queue ready){
