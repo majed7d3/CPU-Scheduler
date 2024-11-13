f00990007baf735dfc471466fca1d9f62f72ee7a
majed <160038172+majed7d3@users.noreply.github.com>
2024-11-13 19:31:52 +0300

    Add files via upload

---
+++ code/operatingsystem.java
@@ -0,0 +1,120 @@
+public class operatingsystem  {
+    private boolean bitMod; //false is kernel mode true is user mode
+    private int memory;
+
+    public operatingsystem(){
+        memory = 1024;
+        bitMod = true;
+    }
+
+    public void switchMod(){
+        bitMod = !bitMod;
+    }
+
+    public PCB create(int id, int burst, int memory){
+        PCB process = null;
+        if(!bitMod){
+            process = new PCB(id, burst, memory);
+            this.memory = this.memory + memory;
+        }
+        return process;
+    }
+
+    public PCB Terminate(PCB process){
+        if(!bitMod){
+            process.setState(state.finish);
+            this.memory = this.memory - memory;
+        }
+        return process;
+    }
+
+    public boolean allocate(int memory){
+        if(!bitMod)
+            this.memory = this.memory - memory;
+        if(this.memory >= 0)
+            return true;
+        return false;
+    }
+
+    public boolean deallocate(int memory){
+        if(!bitMod)
+            this.memory = this.memory + memory;
+        if(this.memory <= 1024)
+            return true;
+        return false;
+    }
+
+    public int getId(PCB process){
+        if(!bitMod){
+            return process.getId();
+        }
+        return -1;
+    }
+
+    public state getState(PCB process){
+        if(!bitMod){
+            return process.getState();
+        }
+        return null;
+    }
+
+    public int getBurst(PCB process){
+        if(!bitMod){
+            return process.getBurst();
+        }
+        return -1;
+    }
+
+    public int getRemain(PCB process){
+        if(!bitMod){
+            return process.getRemain();
+        }
+        return -1;
+    }
+
+    public int getMemory(PCB process){
+        if(!bitMod){
+            return process.getMemory();
+        }
+        return -1;
+    }
+
+    public int getTurnaround(PCB process){
+        if(!bitMod){
+            return process.getTurnaround();
+        }
+        return -1;
+    }
+
+    public int getWait(PCB process){
+        if(!bitMod){
+            return process.getWait();
+        }
+        return -1;
+    }
+
+    public void setState(PCB process, state state) {
+		if(!bitMod){
+            process.setState(state);
+        }
+	}
+
+    public void setTurnaround(PCB process, int turnaround) {
+		if(!bitMod){
+            process.setTurnaround(turnaround);
+        }
+	}
+
+	public void setWait(PCB process, int wait) {
+		if(!bitMod){
+            process.setWait(wait);
+        }
+	}
+
+    public int memoryAllocate() {
+        if(!bitMod)
+            return 1024 - memory;
+        return -1;
+    }
+
+}
---
+++ code/systemcall.java
@@ -0,0 +1,125 @@
+public class systemcall  {
+    private operatingsystem os;
+
+    public systemcall(operatingsystem os){
+        this.os = os;
+    }
+
+    //to creat a process
+    public PCB createProcess(int id, int burst, int memory){ 
+        os.switchMod();
+        PCB process = os.create(id,burst,memory);
+        os.switchMod();
+        return process;
+    }
+
+    //to terminate a process
+    public PCB TerminateProcess(PCB process){
+        os.switchMod();
+        os.Terminate(process);
+        os.switchMod();
+        return process;
+    }
+
+    //to allocate the memory
+    public boolean allocateMemory(int memory){
+        os.switchMod();
+        boolean flag = os.allocate(memory);
+        os.switchMod();
+        return flag;
+    }
+
+    //to deallocate the memory
+    public boolean deallocateMemory(int memory){
+        os.switchMod();
+        boolean flag = os.deallocate(memory);
+        os.switchMod();
+        return flag;
+    }
+
+    //get id of the process
+    public int getId(PCB process){
+        os.switchMod();
+        int get = os.getId(process);
+        os.switchMod();
+        return get;
+    }
+
+    //get state of the process
+    public state getState(PCB process){
+        os.switchMod();
+        state get = os.getState(process);
+        os.switchMod();
+        return get;
+    }
+
+    //get burst time of the process
+    public int getBurst(PCB process){
+        os.switchMod();
+        int get = os.getBurst(process);
+        os.switchMod();
+        return get;
+    }
+
+    //get remain time of the process
+    public int getRemain(PCB process){
+        os.switchMod();
+        int get = os.getRemain(process);
+        os.switchMod();
+        return get;
+    }
+
+    //get the memory of the process
+    public int getMemory(PCB process){
+        os.switchMod();
+        int get = os.getMemory(process);
+        os.switchMod();
+        return get;
+    }
+
+    //get turnaround time of the process
+    public int getTurnaround(PCB process){
+        os.switchMod();
+        int get = os.getTurnaround(process);
+        os.switchMod();
+        return get;
+    }
+
+    //get Waiting time of the process
+    public int getWait(PCB process){
+        os.switchMod();
+        int get = os.getWait(process);
+        os.switchMod();
+        return get;
+    }
+
+    //set state of the process
+    public void setState(PCB process, state state){
+		os.switchMod();
+        os.setState(process, state);
+        os.switchMod();
+	}
+
+    //set turnaround time of the process
+    public void setTurnaround(PCB process, int turnaround){
+		os.switchMod();
+        os.setTurnaround(process, turnaround);
+        os.switchMod();
+	}
+
+    //set Waiting time of the process
+	public void setWait(PCB process, int wait){
+		os.switchMod();
+        os.setWait(process, wait);
+        os.switchMod();
+	}
+
+    //returns the amount of memory that is used
+    public int memoryAllocate(){
+        os.switchMod();
+        int allocate = os.memoryAllocate();
+        os.switchMod();
+        return allocate;
+    }
+
+}
