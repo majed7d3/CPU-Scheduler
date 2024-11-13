18801b852ca7df7f89a34264977919b513a01c4a
majed <160038172+majed7d3@users.noreply.github.com>
2024-11-13 20:00:45 +0300

    Update queue.java

--- code/queue.java
+++ code/queue.java
@@ -1,33 +1,43 @@
 public class queue {
-    private node head, tail;
+    private node head;
     private int size;
 
-    public queue(){
-        head = tail = null;
-        size = 0;
-    }
+    public PQueue() {
+		head = null;
+		size = 0;
+	}
+
 
     public int length(){
         return size;
     }
 
-    public void enqueue(PCB job) {
-		if(tail == null){
-			head = tail = new node(job);
+	//burstTook is useed for calculate the remaining time, remaining = burst_time - burstTook
+    public void enqueue(PCB job, int burstTook, systemcall syscall) { 
+		node tmp = new node(job);
+		syscall.theRemain(job, burstTook);
+		if((size == 0) || (syscall.getRemain(job) > syscall.getRemain(head.process))) {
+			tmp.next = head;
+			head = tmp;
 		}
 		else {
-			tail.next = new node(job);
-			tail = tail.next;
+			node p = head;
+			node q = null;
+			while((p != null) && (syscall.getRemain(job) <= syscall.getRemain(p.process))) {
+				q = p;
+				p = p.next;
+			}
+			tmp.next = p;
+			q.next = tmp;
 		}
 		size++;
 	}
 
-    public PCB serve() {
+
+    public PCB serve(){
 		PCB job = head.process;
 		head = head.next;
 		size--;
-		if(size == 0)
-			tail = null;
 		return job;
 	}
 }
