078485c1a2abd6e25c587a1e4747acacf175bec7
majed <160038172+majed7d3@users.noreply.github.com>
2024-11-14 04:40:21 +0300

    Update queue.java

--- code/queue.java
+++ code/queue.java
@@ -1,16 +1,19 @@
 public class queue {
-    private node head, tail;
-    private int size;
+    private node head, tail; //head and taill of the queue
+    private int size; //size of the queue
 
+	//Constructor
     public queue(){
         head = tail = null;
         size = 0;
     }
 
+	//return the size
     public int length(){
         return size;
     }
 
+	//to add to the queue
     public void enqueue(PCB job) {
 		if(tail == null){
 			head = tail = new node(job);
@@ -22,6 +25,7 @@ public class queue {
 		size++;
 	}
 
+	//return the currnet node
     public PCB serve() {
 		PCB job = head.process;
 		head = head.next;
