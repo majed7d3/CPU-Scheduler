2eb7d33cf2eaa62e1798c8e06b398c543f94982e
majed <160038172+majed7d3@users.noreply.github.com>
2024-11-14 04:36:17 +0300

    Update PCB.java

--- code/PCB.java
+++ code/PCB.java
@@ -1,12 +1,13 @@
 public class PCB {
-    private int id;
-    private state state;
-    private int burst; //burst time
-    private int remain; //the renainging burst time
-    private int memory; //in MB
-    private int turnaround; //turnaround time
-    private int wait; //waiting time
+    private int id; //the id of the process
+    private state state;//the state of the process
+    private int burst; //burst time of process
+    private int remain; //the remaining time of the process
+    private int memory; //the memory need it for the process
+    private int turnaround; //turnaround time of the process
+    private int wait; //waiting time of the process
 
+    //Constructor
     public PCB(int id, int burst, int memory){
         this.id = id;
         this.burst = remain = burst;
@@ -15,47 +16,58 @@ public class PCB {
         state = state.waiting;
     }
 
+    //get id
     public int getId() {
         return id;
     }
 
+    //get state
     public state getState() {
         return state;
     }
 
+    //get burst time
     public int getBurst() {
         return burst;
     }
 
+    //get remaining time
     public int getRemain() {
         return remain;
     }
 
+    //get memory
     public int getMemory() {
         return memory;
     }
 
+    //get turnaround
     public int getTurnaround() {
         return turnaround;
     }
 
+    //get waiting time
     public int getWait() {
         return wait;
     }
-	
-	public void setState(state state) {
+
+    //set state
+    public void setState(state state) {
 		this.state = state;
 	}
-    
-	public void setTurnaround(int turnaround) {
+
+    //set turnaround
+    public void setTurnaround(int turnaround) {
 		this.turnaround = turnaround;
 	}
 
+    //set waiting time
 	public void setWait(int wait) {
 		this.wait = wait;
 	}
-    
-        public void theRemain(int burstTook) {
+
+    //set remaining time by deleteing the time that has been done
+    public void theRemain(int burstTook) {
         remain = remain - burstTook;
     }
 }
