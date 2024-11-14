e58d335263eec1e1ca7d7591b58cd58809defbf2
majed <160038172+majed7d3@users.noreply.github.com>
2024-11-14 04:37:46 +0300

    Update PQueue.java

--- code/PQueue.java
+++ code/PQueue.java
@@ -1,29 +1,32 @@
 public class PQueue {
-    private node head;
-    private int size;
+    private node head; //head of the queue
+    private int size; //size of the queue
 
+	//Constructor
     public PQueue() {
 		head = null;
 		size = 0;
 	}
 
-
+	//return the size
     public int length(){
         return size;
     }
 
-	//burstTook is useed for calculate the remaining time, remaining = burst_time - burstTook
+	//to add to the queue
+	//burstTook is useed for calculate the remaining time, remaining = burst time - burstTook, 0 otherwise
+	//syscall for calling system calls
     public void enqueue(PCB job, int burstTook, systemcall syscall) { 
 		node tmp = new node(job);
 		syscall.theRemain(job, burstTook);
-		if((size == 0) || (syscall.getRemain(job) > syscall.getRemain(head.process))) {
+		if((size == 0) || (syscall.getRemain(job) < syscall.getRemain(head.process))) {
 			tmp.next = head;
 			head = tmp;
 		}
 		else {
 			node p = head;
 			node q = null;
-			while((p != null) && (syscall.getRemain(job) <= syscall.getRemain(p.process))) {
+			while((p != null) && (syscall.getRemain(job) >= syscall.getRemain(p.process))) {
 				q = p;
 				p = p.next;
 			}
@@ -33,7 +36,7 @@ public class PQueue {
 		size++;
 	}
 
-
+	//return the current node
     public PCB serve(){
 		PCB job = head.process;
 		head = head.next;
