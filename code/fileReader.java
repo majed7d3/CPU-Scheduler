770165b3f64b22993fe15304306200579b8fd8a5
majed <160038172+majed7d3@users.noreply.github.com>
2024-11-14 04:39:38 +0300

    Update fileReader.java

--- code/fileReader.java
+++ code/fileReader.java
@@ -4,48 +4,56 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class fileReader implements Runnable{
-    private boolean flag;
-    private String fileName;
-    private PQueue jabPQueue;
-    private queue jabQueue;
-    private systemcall syscall;
+    private boolean isPriority; //to know if the queue is priority or not (true for priority, false for no priority)
+    private String fileName; //the file name of the input file
+    private PQueue priorityJobQueue; //priority job queue (the priority is on the burst time from smallest to largest)
+    private queue jobQueue; //job queue
+    private systemcall syscall; //for calling system calls
 
 
-    public fileReader(PQueue jabPQueue,String fileName, systemcall syscall){
-        flag = false;
+    //Constructor if the queue is priority
+    public fileReader(PQueue priorityJobQueue,String fileName, systemcall syscall){
+        isPriority = true;
         this.fileName = fileName;
-        this.jabPQueue = jabPQueue;
+        this.priorityJobQueue = priorityJobQueue;
         this.syscall = syscall;
     }
 
-    public fileReader(queue jabQueue,String fileName, systemcall syscall){
-        flag = true;
+    //Constructor for a queue 
+    public fileReader(queue jobQueue,String fileName, systemcall syscall){
+        isPriority = false;
         this.fileName = fileName;
-        this.jabQueue = jabQueue;
+        this.jobQueue = jobQueue;
         this.syscall = syscall;
     }
 
 
+    //for reading the file job.txt
+    //the job.txt need to be in the same folder as the code
+    //the job.txt need to have this strusture id:burst_time;Memory
+    //the job.txt have to begin with (id:burst_time;Memory) and end with (id:burst_time;Memory)
     @Override
     public void run() {
         File file = new File(fileName);
         try (FileReader reader = new FileReader(file)) {
             BufferedReader buffer = new BufferedReader(reader);
-            String line = ""; 
-            while((line = buffer.readLine()) != null){
-                String[] dataJab = new String[3];
-                dataJab[0] = (line.split(":"))[0];
-                dataJab[1] = ((line.split(":"))[1].split(";"))[0];
-                dataJab[2] = ((line.split(":"))[1].split(";"))[1]; 
-                int id = Integer.parseInt(dataJab[0]);
-                int burst = Integer.parseInt(dataJab[1]);
-                int memory = Integer.parseInt(dataJab[2]);
-                    if(flag)
-                        jabQueue.enqueue(syscall.createProcess(id, burst, memory));
+            String line = ""; //to save the current line
+            while((line = buffer.readLine()) != null){ //while the current line is not null
+                //this for spliting the line to have the id, burst time, Memory
+                String[] datajob = new String[3];
+                datajob[0] = (line.split(":"))[0];
+                datajob[1] = ((line.split(":"))[1].split(";"))[0];
+                datajob[2] = ((line.split(":"))[1].split(";"))[1]; 
+                int id = Integer.parseInt(datajob[0]);
+                int burst = Integer.parseInt(datajob[1]);
+                int memory = Integer.parseInt(datajob[2]);
+                    if(!isPriority) //to check if we add the Process queue to or a priority queue
+                        jobQueue.enqueue(syscall.createProcess(id, burst, memory));
                     else
-                        jabPQueue.enqueue(syscall.createProcess(id, burst, memory), 0, syscall);
+                        priorityJobQueue.enqueue(syscall.createProcess(id, burst, memory), 0, syscall);
             }
         } catch (IOException e) {
+            //if we can not find the file
             System.out.println("no file found.");
         }
     }
