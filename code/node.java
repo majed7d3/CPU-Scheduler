ff0335e63535399e6e15d17b87fbae9df7580311
majed <160038172+majed7d3@users.noreply.github.com>
2024-11-14 04:39:49 +0300

    Update node.java

--- code/node.java
+++ code/node.java
@@ -1,29 +1,35 @@
 public class node {
-    public PCB process;
-    public node next;
+    public PCB process; //the PCB of a process
+    public node next; //the next in the linklist
 
+    //Constructor
     public node(){
         process = null;
         next = null;
     }
 
+    //copy Constructor
     public node(PCB process) {
         this.process = process;
         next = null;
     }
 
+    //get next node
     public node getNext() {
         return next;
     }
 
+    //get the node Process
     public PCB getProcess() {
         return process;
     }
 
+    //set next node
     public void setNext(node next) {
         this.next = next;
     }
 
+    //set the node Process
     public void setProcess(PCB process) {
         this.process = process;
     }
