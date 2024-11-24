52bfda159d42c7f8cf11ce2df140731b617c2cfb
AbdullahGham <95870803+AbdullahGham@users.noreply.github.com>
2024-11-24 18:09:19 +0300

    Update appRun.java

--- code/appRun.java
+++ code/appRun.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
-
 public class appRun {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("choose the scheduling algorithm");
+        System.out.println("Choose the scheduling algorithm:");
         System.out.println("1. First-Come-First-Serve (FCFS).");
-        System.out.println("2. Round-Robin (RR), the quantum = 8ms");
-        System.out.println("3. Shortest Job First");
+        System.out.println("2. Round-Robin (RR), the quantum = 8ms.");
+        System.out.println("3. Shortest Job First.");
         int algorithm = input.nextInt();
 
-        //initializing
         PQueue priorityJobQueue = new PQueue();
         queue jobQueue = new queue();
         queue readyQueue = new queue();
@@ -21,45 +19,36 @@ public class appRun {
         fileReader reader;
         Thread jobThread;
         readyJob ready;
-        
-        //to see if it need a priority queue or not
-        if(algorithm == 3){
-            //initializing the reader thread
-            reader = new fileReader(priorityJobQueue,"job.txt", syscall);
+
+        // Initialize reader and ready threads based on algorithm
+        if (algorithm == 3) {
+            reader = new fileReader(priorityJobQueue, "./job.txt", syscall);
             jobThread = new Thread(reader);
-            //starting the reader thread
             jobThread.start();
-            //initializing the ready thread
             ready = new readyJob(priorityJobQueue, readyQueue, cancelQueue, syscall, jobThread);
-        }
-        else{
-            //initializing the reader thread
-            reader = new fileReader(jobQueue,"job.txt", syscall);
+        } else {
+            reader = new fileReader(jobQueue, "./job.txt", syscall);
             jobThread = new Thread(reader);
-            //starting the reader thread
             jobThread.start();
-            //initializing the ready thread
             ready = new readyJob(jobQueue, readyQueue, cancelQueue, syscall, jobThread);
         }
         Thread readyThread = new Thread(ready);
-        //starting the ready thread
         readyThread.start();
-        //starting the cpu scheduler
-        Schedulers scheduler = new Schedulers(finishQueue, syscall, readyThread);
+
+        Schedulers scheduler = new Schedulers(finishQueue, syscall, readyThread, result);
+
         switch (algorithm) {
             case 1:
                 scheduler.First_Come_First_Serve(readyQueue);
                 break;
-            
             case 2:
                 scheduler.Round_Robin(readyQueue);
                 break;
-            
             case 3:
                 scheduler.Shortest_Job_First(readyQueue);
                 break;
         }
 
-           result.displayResults(); 
+        result.displayResults();
     }
 }
