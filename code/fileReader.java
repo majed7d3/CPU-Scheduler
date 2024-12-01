38da57a29aae44d1d21a9eaf031c1482ae0d8daf
majed <160038172+majed7d3@users.noreply.github.com>
2024-12-01 17:39:43 +0300

    Update fileReader.java

--- code/fileReader.java
+++ code/fileReader.java
@@ -4,12 +4,13 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class fileReader implements Runnable {
-    private boolean isPriority;
-    private String fileName;
-    private PQueue priorityJobQueue;
-    private queue jobQueue;
-    private systemcall syscall;
+    private boolean isPriority; //to know if the queue is priority or not (true for priority, false for no priority)
+    private String fileName; //the file name of the input file
+    private PQueue priorityJobQueue; //priority job queue (the priority is on the burst time from smallest to largest)
+    private queue jobQueue; //job queue
+    private systemcall syscall; //for calling system calls
 
+    //Constructor if it is priority queue
     public fileReader(PQueue priorityJobQueue, String fileName, systemcall syscall) {
         this.isPriority = true;
         this.fileName = fileName;
@@ -17,6 +18,7 @@ public class fileReader implements Runnable {
         this.syscall = syscall;
     }
 
+    //Constructor 
     public fileReader(queue jobQueue, String fileName, systemcall syscall) {
         this.isPriority = false;
         this.fileName = fileName;
@@ -24,12 +26,17 @@ public class fileReader implements Runnable {
         this.syscall = syscall;
     }
 
+    //for reading the file job.txt
+    //the job.txt need to be in the same folder as the code
+    //the job.txt need to have this strusture id:burst_time;Memory
+    //the job.txt have to begin with (id:burst_time;Memory) and end with (id:burst_time;Memory)
     @Override
     public void run() {
         File file = new File(fileName);
         try (BufferedReader buffer = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = buffer.readLine()) != null) {
+            String line; //to save the current line
+            while ((line = buffer.readLine()) != null) { //while the current line is not null
+                //this for spliting the line to have the id, burst time, Memory
                 String[] datajob = new String[3];
                 datajob[0] = line.split(":")[0];
                 datajob[1] = line.split(":")[1].split(";")[0];
@@ -40,15 +47,17 @@ public class fileReader implements Runnable {
                 int memory = Integer.parseInt(datajob[2]);
                 PCB process = syscall.createProcess(id, burst, memory);
 
-                if (process != null) {
-                    if (isPriority) {
+                if (process != null) { //check if the Process is null
+                    if (isPriority) { //to check if we add the Process to a queue or a priority queue
                         priorityJobQueue.enqueue(process, 0, syscall);
                     } else {
                         jobQueue.enqueue(process);
                     }
                 }
             }
+            buffer.close();
         } catch (IOException e) {
+            //if we can not find the file
             System.out.println("File not found: " + fileName);
         }
     }
