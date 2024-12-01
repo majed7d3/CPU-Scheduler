45ae643b2ff36735a88d071e78384b5694799849
majed <160038172+majed7d3@users.noreply.github.com>
2024-12-01 17:39:30 +0300

    Update appRun.java

--- code/appRun.java
+++ code/appRun.java
@@ -8,17 +8,17 @@ public class appRun {
         System.out.println("3. Shortest Job First.");
         int algorithm = input.nextInt();
 
-        PQueue priorityJobQueue = new PQueue();
-        queue jobQueue = new queue();
-        queue readyQueue = new queue();
-        queue finishQueue = new queue();
-        queue cancelQueue = new queue();
+        //initializing
+        PQueue priorityJobQueue = new PQueue(); //priority job queue from the file
+        queue jobQueue = new queue(); //job queue from the file
+        queue readyQueue = new queue(); //a queue for jobs in the memory
+        queue cancelQueue = new queue(); //a queue for canceled jobs (bigger then the memory)
         operatingsystem os = new operatingsystem();
         systemcall syscall = new systemcall(os);
-        SchedulerResult result = new SchedulerResult();
-        fileReader reader;
-        Thread jobThread;
-        readyJob ready;
+        SchedulerResult result = new SchedulerResult(); //for printing
+        fileReader reader; //reading from the file
+        Thread jobThread; //thread for reading from the file
+        readyJob ready; //for ready job queue (checking the memory)
 
         // Initialize reader and ready threads based on algorithm
         if (algorithm == 3) {
@@ -35,8 +35,9 @@ public class appRun {
         Thread readyThread = new Thread(ready);
         readyThread.start();
 
-        Schedulers scheduler = new Schedulers(finishQueue, syscall, readyThread, result);
+        Schedulers scheduler = new Schedulers(syscall, readyThread, result);
 
+        //chose the scheduler
         switch (algorithm) {
             case 1:
                 scheduler.First_Come_First_Serve(readyQueue);
@@ -49,6 +50,22 @@ public class appRun {
                 break;
         }
 
+        //print the jobs + gantt chart
         result.displayResults();
+
+        //print any canceled jobs
+        int length = cancelQueue.length();
+        if(length != 0){
+            System.out.println("\nThe canceled jobs:");
+            
+            for(int i = 0; i < length; i++){
+                PCB job = cancelQueue.serve();
+                if(i+1 == length){
+                    System.out.print("P"+syscall.getId(job));
+                    break;
+                }
+                System.out.print("P"+syscall.getId(job)+", ");
+            }
+        }
     }
 }
